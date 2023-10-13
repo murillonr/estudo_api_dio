0769763bb5a4b20daf4684d4375ef5f65235a91f
Murillo Negreiros <murillonr89@gmail.com>
2023-10-12 21:07:51 -0300

    configurando o PostMaping

--- santander-bootcamp-2023/src/main/java/me/dio/domain/controller/UserController.java
+++ santander-bootcamp-2023/src/main/java/me/dio/domain/controller/UserController.java
@@ -3,10 +3,10 @@ package me.dio.domain.controller;
 import me.dio.domain.model.User;
 import me.dio.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("users")
@@ -22,5 +22,15 @@ public class UserController {
     public ResponseEntity<User> findById(@PathVariable Long id){
         var user = userService.findById(id);
         return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User userToCreate){
+        var userCreated = userService.create(userToCreate);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
     }
 }
